1f2008d9ee7392ef3bad71ca75fa2642d14aa0f3
Andrea Johnson <ajohnson1@ajohnson-kcmo.vml.lan>
2019-03-01 14:07:39 -0600

    Added root component; Started writing template for product list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+//3. import the component decorator
+import { Component } from "@angular/core";
 
+//2. define component decorator above the class
 @Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'pm-root', //find this custom html tag in index.html
+  template: `
+  <div><h1>{{pageTitle}}</h1>
+    <div>I made my first component!</div>
+  </div>`
 })
+
+//1. start by creating class
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = "American Public Mysteries";
 }
