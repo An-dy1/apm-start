7a26f856cefe2f90b410131962c1adcdf1299639
Andrea Johnson <ajohnson1@ajohnson-kcmo.vml.lan>
2019-03-04 12:14:32 -0600

    Implemented databinding with ngModel and event binding; added pipes to properties

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,42 +1,54 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-    selector: "pm-products",
-    templateUrl: './product-list.component.html'
+  selector: "pm-products",
+  templateUrl: "./product-list.component.html"
 })
 
 export class ProductListComponent {
-    pageTitle: string = "Mysteries 5000";
-    products: any[] = [
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2016",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-          },
-          {
-            "productId": 8,
-            "productName": "Saw",
-            "productCode": "TBX-0022",
-            "releaseDate": "May 15, 2016",
-            "description": "15-inch steel blade hand saw",
-            "price": 11.55,
-            "starRating": 3.7,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
-          },
-          {
-            "productId": 10,
-            "productName": "Video Game Controller",
-            "productCode": "GMG-0042",
-            "releaseDate": "October 15, 2015",
-            "description": "Standard two-button video game controller",
-            "price": 35.95,
-            "starRating": 4.6,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
-          }
-        ];
+  pageTitle: string = "Mysteries 5000";
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false; // define a class property that keeps track of whether the images are currently displayed
+  listFilter: string = 'saw';
+  products: any[] = [
+    {
+      productId: 5,
+      productName: "Hammer",
+      productCode: "TBX-0048",
+      releaseDate: "May 21, 2016",
+      description: "Curved claw steel hammer",
+      price: 8.9,
+      starRating: 4.8,
+      imageUrl:
+        "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+    },
+    {
+      productId: 8,
+      productName: "Saw",
+      productCode: "TBX-0022",
+      releaseDate: "May 15, 2016",
+      description: "15-inch steel blade hand saw",
+      price: 11.55,
+      starRating: 3.7,
+      imageUrl:
+        "https://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
+    },
+    {
+      productId: 10,
+      productName: "Video Game Controller",
+      productCode: "GMG-0042",
+      releaseDate: "October 15, 2015",
+      description: "Standard two-button video game controller",
+      price: 35.95,
+      starRating: 4.6,
+      imageUrl:
+        "https://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
+    }
+  ];
+  
+  toggleImage(): void { // no return type, so declare as void
+    this.showImage = !this.showImage;
+  }
+    // build the method that the event binding will call; goes after all properties are declared by convention
 }
